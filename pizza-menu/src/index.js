bb2f78940a74ee911bf57f9ccb174588fb08dc24
AlainDLC <jd_alain@hotmail.com>
2024-07-24 20:52:00 +0200

    refactor logic

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -66,28 +66,34 @@ function App() {
 function Header() {
   return (
     <header className="header">
-      <h1>Fast React Pizza Co</h1>{" "}
+      <h1>Fast React Pizza Co</h1>
     </header>
   );
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
-
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza key={pizza.id} pizza={pizza} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza key={pizza.id} pizza={pizza} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later</p>
+      )}
     </main>
   );
 }
 
 function Pizza(props) {
+  if (props.pizza.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className="pizza ">
       <img src={props.pizza.photoName} alt="pizzasort" />
       <h3>{props.pizza.name}</h3>
       <p>Ingredients {props.pizza.ingredients}</p>
@@ -100,14 +106,20 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
-  const IsOpend =
-    hour >= openHour && hour <= closeHour
-      ? "We're are currently open"
-      : "Sorry we are closed";
+  const IsOpend = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
-      {IsOpend} {new Date().toLocaleTimeString()}
+      {IsOpend ? (
+        <div className="order">
+          <p>We're Open until {closeHour}:00 Come visit us or order inline</p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
+        </p>
+      )}
     </footer>
   );
 }
